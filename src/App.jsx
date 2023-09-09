0e8d4b503a3186b93adbe8c8b8ff24147ce657d0
Dion Charles <dionr_charles@hotmail.com>
2023-09-08 20:52:21 -0400

    add gif

--- src/App.jsx
+++ src/App.jsx
@@ -52,3 +52,4 @@ export default function App() {
     </Router>
   );
 }
+
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images';
+
 import { Link, useLocation } from 'react-router-dom';
 
 
