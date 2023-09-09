8d4d34af2bb451dc2801571877eaa5140802136f
Dion Charles <dionr_charles@hotmail.com>
2023-09-08 21:03:51 -0400

    moved images folder into components folder

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/blackwhitesimplemusiclogo.gif';
+import logo from './images/blackwhitesimplemusiclogo.gif';
 import { Link, useLocation } from 'react-router-dom';
 
 
