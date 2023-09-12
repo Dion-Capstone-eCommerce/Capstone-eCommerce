4dca52abafde801ca8eb13ddaae27bf5b4bfaf90
Dion Charles <dionr_charles@hotmail.com>
2023-09-11 20:27:15 -0400

    changes to projects.js

--- src/products.js
+++ src/products.js
@@ -1,147 +1,147 @@
-import product1 from "./images/speakers/sonyXS-163ES.png";
-// import product2 from './images/sonyXS-163ES.png'
-// import product3 from './images/sonyXS-163ES.png'
-// import product4 from './images/sonyXS-163ES.png'
-// import product5 from './images/sonyXS-163ES.png'
-// import product6 from './images/sonyXS-163ES.png'
-// import product7 from './images/sonyXS-163ES.png'
-// import product8 from './images/sonyXS-163ES.png'
-// import product9 from './images/sonyXS-163ES.png'
-// import product10 from './images/sonyXS-163ES.png'
-// import product11 from './images/sonyXS-163ES.png'
-// import product12 from './images/sonyXS-163ES.png'
-// import product13 from './images/sonyXS-163ES.png'
-// import product14 from './images/sonyXS-163ES.png'
-// import product15 from './images/sonyXS-163ES.png'
-// import product16 from './images/sonyXS-163ES.png'
+import product1 from "./images/radio/sonyXAV-9500ES.png";
+import product2 from './images/radio/sonyXAV-9000ES.png';
+import product3 from './images/speakers/sonyXS-160ES.png';
+import product4 from './images/speakers/sonyXS-162ES.png';
+import product5 from './images/speakers/sonyXS-163ES.png';
+import product6 from './images/speakers/sonyXS-680ES.png';
+import product7 from './images/speakers/sonyXS-690ES.png';
+import product8 from './images/speakers/sonyXS-692ES.png';
+import product9 from './images/amplifiers/sonyXM-1ES.png';
+import product10 from './images/amplifiers/sonyXM-4ES.png';
+import product11 from './images/amplifiers/sonyXM-5ES.png';
+import product12 from './images/amplifiers/sonyXM-6ES.png';
+import product13 from './images/amplifiers/sonyXM-8ES.png';
+import product14 from './images/subwoofers/sonyXS-W104ES.png';
+import product15 from './images/subwoofers/sonyXS-W122ES.png';
+import product16 from './images/subwoofers/sonyXS-W124ES.png';
 
 export const PRODUCTS = [
     {
         id:1,
-        title:'Sony Mobile ES XS-163ES',
-        price: 499.99,
-        category:'Speakers',
-        description:'6 1/2 inch 3-Way Component Speakers',
+        title:'Sony Mobile ES XAV-9500ES',
+        price: 999.99,
+        category:'Media Receiver',
+        description:'10.1 inch Wireless CarPlay/ Android Auto Media Receiver',
         image: product1, 
     },
-    // {
-    //     id:2,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product2, 
-    // },
-    // {
-    //     id:3,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product3, 
-    // },
-    // {
-    //     id:4,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product4,
-    // },
-    // {
-    //     id:5,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product5,
-    // },
-    // {
-    //     id:6,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product6,
-    // },
-    // {
-    //     id:7,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product7,
-    // },
-    // {
-    //     id:8,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product8,
-    // },
-    // {
-    //     id:9,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product9,
-    // },
-    // {
-    //     id:10,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product10, 
-    // }
-    // {
-    //     id:11,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product11, 
-    // }
-    // {
-    //     id:12,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product12, 
-    // }
-    // {
-    //     id:13,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product13, 
-    // }
-    // {
-    //     id:14,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product14, 
-    // }
-    // {
-    //     id:15,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product15, 
-    // }
-    // {
-    //     id:16,
-    //     title:'...',
-    //     price: 1,
-    //     category:'...',
-    //     description:'...',
-    //     image: product16, 
-    // }
+    {
+        id:2,
+        title:'Sony Mobile ES XAV-9000ES',
+        price: 799.99,
+        category:'Media Receiver',
+        description:'6.75 inch Wireless High-Resolution Media Receiver',
+        image: product2, 
+    },
+    {
+        id:3,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product3, 
+    },
+    {
+        id:4,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product4,
+    },
+    {
+        id:5,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product5,
+    },
+    {
+        id:6,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product6,
+    },
+    {
+        id:7,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product7,
+    },
+    {
+        id:8,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product8,
+    },
+    {
+        id:9,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product9,
+    },
+    {
+        id:10,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product10, 
+    },
+    {
+        id:11,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product11, 
+    },
+    {
+        id:12,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product12, 
+    },
+    {
+        id:13,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product13, 
+    },
+    {
+        id:14,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product14, 
+    },
+    {
+        id:15,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product15, 
+    },
+    {
+        id:16,
+        title:'...',
+        price: 1,
+        category:'...',
+        description:'...',
+        image: product16, 
+    }
 ]
