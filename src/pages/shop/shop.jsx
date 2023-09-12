a335bde8a606679d24f559106ceced03397369c6
Dion Charles <dionr_charles@hotmail.com>
2023-09-12 16:07:46 -0400

    change to shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -9,9 +9,10 @@ export const Shop = () => {
             <div className="shopTitle">
                 <h1>My Shop</h1>
             </div>
-            <div className="products"> {PRODUCTS.map((product) => {
-                return <Product data={product} key={product.id} />;
-            })}
+            <div className="products">
+                 {PRODUCTS.map((product) => (
+                 <Product data={product} />
+            ))}
             </div>
         </div>
     );
